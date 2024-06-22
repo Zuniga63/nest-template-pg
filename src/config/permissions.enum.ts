306c30391dadd28f19b8a90ee71147b125b39339
Andrés Felipe Zúñiga <andres.zuniga.063@gmail.com>
2024-06-22 18:20:40 -0500

    feat: add permissions enum for user and role permissions

---
+++ src/config/permissions.enum.ts
@@ -0,0 +1,21 @@
+export enum Permission {
+  // ---------------------------------------------------------------
+  // USERS PERMISSIONS
+  // ---------------------------------------------------------------
+  READ_USERS = 'READ_USERS',
+  CREATE_NEW_USER = 'CREATE_NEW_USER',
+  UPDATE_USER = 'UPDATE_USER',
+  READ_USER = 'READ_USER',
+  DELETE_USER = 'DELETE_USER',
+
+  // ---------------------------------------------------------------
+  // ROLES PERMISSIONS
+  // ---------------------------------------------------------------
+  CREATE_ROLE = 'CREATE_ROLE',
+  READ_ROLE = 'READ_ROLE',
+  UPDATE_ROLE = 'UPDATE_ROLE',
+  DELETE_ROLE = 'DELETE_ROLE',
+  ADD_ROLE_TO_USER = 'ADD_ROLE_TO_USER',
+  REMOVE_ROLE_TO_USER = 'REMOVE_ROLE_TO_USER',
+  UPDATE_ROLE_PERMISSIONS = 'UPDATE_ROLE_PERMISSIONS',
+}
