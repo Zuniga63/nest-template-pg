a990d507f41646fb6881dd20b42b70c56edc3f57
Andrés Felipe Zúñiga <andres.zuniga.063@gmail.com>
2024-07-11 09:28:29 -0500

    refactor: remove unused code in UsersController

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,32 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
-
-import { UsersService } from './users.service';
-import { SecureUser } from './interfaces/secure-user.interface';
-import { GetUser } from '../common/decorators/get-user.decorator';
-import { SwaggerTags } from 'src/config';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { UserDto } from './dto/user.dto';
-import { Auth } from '../auth/decorators';
+import { Controller } from '@nestjs/common';
 
 @Controller('users')
-@ApiTags(SwaggerTags.Users)
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-
-  // * ----------------------------------------------------------------------------------------------------------------
-  // * GET PROFILE
-  // * ----------------------------------------------------------------------------------------------------------------
-  @Get('/profile')
-  @Auth()
-  @ApiOperation({
-    summary: 'Get user profile',
-    description: 'This end recover the info of one user',
-  })
-  @ApiOkResponse({
-    description: 'User Info',
-    type: UserDto,
-  })
-  getProfile(@GetUser() user: SecureUser) {
-    return user;
-  }
+  // TODO: Inject the UsersService
 }
